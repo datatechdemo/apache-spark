11ac2b39c1ac58c2a1d90f2d6568adcf6c1ddd4e
datatechdemo <82328348+datatechdemo@users.noreply.github.com>
2021-05-14 22:26:29 -0400

    Update no-spark-session.py

--- Code/no-spark-session.py
+++ Code/no-spark-session.py
@@ -1,13 +1,11 @@
 """
-Shows a simple application being executed in Python without a session
-
+Shows a simple application being executed in spark without a session
 Run with spark2-submit simple_with_session.py
-
-Auther- Anand Kumar
+Auther- Andy
 """
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("Simple with Session").master("yarn").getOrCreate()
+spark = SparkSession.builder.appName("Simple with Session").getOrCreate()
 
 print "*** Testing simple_with_session.py ***"
 print "*** Application name: " + spark.sparkContext.appName + " / Version: " + spark.version + " ***"
