0230d4e3b29c29df8955de8fec9e5ddcf2f3fa0b
datatechdemo <82328348+datatechdemo@users.noreply.github.com>
2021-05-15 18:46:16 -0400

    Update basic.py

--- Code/basic.py
+++ Code/basic.py
@@ -6,14 +6,17 @@ Author- Andy
 """
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
+from pyspark.sql.functions import * 
 
-
-def readdata():
+def workwithdf(spark):
   # Loading DataFrames: CSV method 1 using spark.read.csv('path')
-  player_df =spark.read.option('header', True).csv('gs://dataproc-staging-us-east1-548550014762-rie5an4z/notebooks/jupyter/player.csv')
+  player_df =spark.read.option('header', True).option('inferSchema', True).csv('gs://dataproc-staging-us-east1-548550014762-rie5an4z/notebooks/jupyter/player.csv')
   player_df.show(5, False)
-  display(player_df)
   player_df.printSchema()
+  
+  player_attr_df =spark.read.option('header', True).option('inferSchema', True).csv('gs://dataproc-staging-us-east1-548550014762-rie5an4z/notebooks/jupyter/player_attributes.csv')
+  player_attr_df.show(5, False)
+  player_attr_df.printSchema()
   
   # Selecting multiple columns
   df = player_df.select('player_api_id')
@@ -23,8 +26,8 @@ def readdata():
   
   # Add and update columns # notes Spark withColumn() is a DataFrame function that is used to add a new column to DataFrame, change the value of an existing column, convert the datatype of a column, derive a new column from an existing column
   
-  player_df.withColumn("country", lit("USA"))
-  player_df.withColumn("height", col('height')/2.54)
+  player_df = player_df.withColumn("country", lit("USA"))
+  player_df = player_df.withColumn("height", col('height')/2.54)
   
   # Rename columns #notes Spark has a withColumnRenamed() function on DataFrame to change a column name. This is the most straight forward approach; this function takes two parameters; the first is your existing column name and the second is the new column name you wish for.
   player_df.withColumnRenamed('height', 'height_in_cms')
@@ -34,7 +37,7 @@ def readdata():
   player_df = player_df.drop('id', 'player_fifa_api_id')
   player_df.columns
   
-  player_attributes = player_attributes.drop(
+  player_attr_df = player_attr_df.drop(
     'id', 
     'player_fifa_api_id', 
     'preferred_foot',
@@ -47,34 +50,49 @@ def readdata():
     'aggression',
     'short_passing',
     'potential'
-)
-player_attributes.columns
+   )
+  player_attr_df.columns
   
   # When and otherwise 
+  player_df = player_df.withColumn("region", when(col("country") == "USA","North America").otherwise("Unknown"))
   
   # Where and Filter
   
   # Distinct
   
+  player_attr_df.select('player_api_id')\
+                   .distinct()\
+                   .count()
+  
   # Sort
+ 
+  player_df = player_df.orderBy('player_api_id')
+  player_df.show(5, False)
   
   # Aggregation
   
+  player_attr_df = player_attr_df.groupBy('player_api_id')\
+                       .agg({
+                           'finishing':"avg",
+                           "shot_power":"avg",
+                           "acceleration":"avg"
+                       })
+  
   # join
   
-  # Union
-  
-  # datefucntion
-  
-  # string function 
-  
-  # Windows 
-  
-  # explode 
+  join_df = player_df.join( player_attr_df, player_df.player_api_id == player_attr_df.player_api_id , 'inner')
+ 
   
   # partitioning 
+  # No.1 repartition() & coalesce()
+  # No. 2 partitionBy()
+  join_df = join_df.repartition(4)
   
-
+  '''df.write.option("header",True) \
+            .partitionBy("year") \
+            .mode("overwrite") \
+            .csv("/tmp/zipcodes-state")
+   '''
  
 if __name__ == "__main__":
   spark = SparkSession \
@@ -82,6 +100,6 @@ if __name__ == "__main__":
     .appName("Analyzing soccer players") \
     .getOrCreate()
   
-  def readdata(spark)
+  workwithdf(spark)
   
   spark.stop()
