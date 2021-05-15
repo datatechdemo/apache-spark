521de7789c583ba26f257eaa606e099fc5b62c69
datatechdemo <82328348+datatechdemo@users.noreply.github.com>
2021-05-15 17:53:10 -0400

    Update basic.py

--- Code/basic.py
+++ Code/basic.py
@@ -18,14 +18,37 @@ def readdata():
   # Selecting multiple columns
   df = player_df.select('player_api_id')
   df.show(5,False)
-  df = 
+  player_df.select('id','player_api_id').show(5,False)
   
   
-  # Add and update columns
+  # Add and update columns # notes Spark withColumn() is a DataFrame function that is used to add a new column to DataFrame, change the value of an existing column, convert the datatype of a column, derive a new column from an existing column
   
-  # Rename columns
+  player_df.withColumn("country", lit("USA"))
+  player_df.withColumn("height", col('height')/2.54)
+  
+  # Rename columns #notes Spark has a withColumnRenamed() function on DataFrame to change a column name. This is the most straight forward approach; this function takes two parameters; the first is your existing column name and the second is the new column name you wish for.
+  player_df.withColumnRenamed('height', 'height_in_cms')
   
   # Drop columns 
+  
+  player_df = player_df.drop('id', 'player_fifa_api_id')
+  player_df.columns
+  
+  player_attributes = player_attributes.drop(
+    'id', 
+    'player_fifa_api_id', 
+    'preferred_foot',
+    'attacking_work_rate',
+    'defensive_work_rate',
+    'crossing',
+    'jumping',
+    'sprint_speed',
+    'balance',
+    'aggression',
+    'short_passing',
+    'potential'
+)
+player_attributes.columns
   
   # When and otherwise 
   
