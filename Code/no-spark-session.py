6bccf1132913bdcfc9d75a2202cd894cbcca121b
datatechdemo <82328348+datatechdemo@users.noreply.github.com>
2021-05-14 22:30:51 -0400

    Update no-spark-session.py

--- Code/no-spark-session.py
+++ Code/no-spark-session.py
@@ -6,7 +6,7 @@ Author- Andy
 from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
-  spark = SparkSession.builder.appName("Simple with Session").getOrCreate()
+  #spark = SparkSession.builder.appName("Simple with Session").getOrCreate()
 
   print "*** Testing simple_with_session.py ***"
   print "*** Application name: " + spark.sparkContext.appName + " / Version: " + spark.version + " ***"
