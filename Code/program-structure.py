8a7f2ec5ac365ef76d5ccaaae4863ce61c578cda
datatechdemo <82328348+datatechdemo@users.noreply.github.com>
2021-05-15 21:58:39 -0400

    Update program-structure.py

--- Code/program-structure.py
+++ Code/program-structure.py
@@ -7,17 +7,10 @@ Author - Andy
 
 #Import libraries
 from pyspark.sql import SparkSession
-#Other libraries
+# Other libraries
 
 def function_example(spark):
-  # +--------+---+---+---+
-    # |    time| id| v1| v2|
-    # +--------+---+---+---+
-    # |20000101|  1|1.0|  x|
-    # |20000102|  1|3.0|  x|
-    # |20000101|  2|2.0|  y|
-    # |20000102|  2|4.0|  y|
-    # +--------+---+---+---+
+# code
 
 
 if __name__ == "__main__":
